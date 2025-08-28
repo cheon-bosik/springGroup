f30654995c0435ac9f3c1614da2440ed592a8a25
ccbbss11126 <ccbbss11126@gmail.com>
2025-08-28 17:52:12 +0900

    20250828_1752 파일업로드

--- src/main/java/board/BoardVO.java
+++ src/main/java/board/BoardVO.java
@@ -13,6 +13,10 @@ public class BoardVO {
 	private int good;
 	private String complaint;
 	
+	private int hour_diff;	// new.gif를 출력하기위한 변수(24시간 내에)
+	private int date_diff;	// 글쓴날짜를 오늘이후는 '날짜표시', 오늘 이전은 '시간/날짜-시간' 표시 하기위한 변후
+	private int replyCnt;		// 댓글의 갯수를 저장하기위한 변수
+	
 	public int getIdx() {
 		return idx;
 	}
@@ -79,11 +83,30 @@ public class BoardVO {
 	public void setComplaint(String complaint) {
 		this.complaint = complaint;
 	}
+	public int getHour_diff() {
+		return hour_diff;
+	}
+	public void setHour_diff(int hour_diff) {
+		this.hour_diff = hour_diff;
+	}
+	public int getDate_diff() {
+		return date_diff;
+	}
+	public void setDate_diff(int date_diff) {
+		this.date_diff = date_diff;
+	}
+	public int getReplyCnt() {
+		return replyCnt;
+	}
+	public void setReplyCnt(int replyCnt) {
+		this.replyCnt = replyCnt;
+	}
 	
 	@Override
 	public String toString() {
 		return "BoardVO [idx=" + idx + ", mid=" + mid + ", nickName=" + nickName + ", title=" + title + ", content="
 				+ content + ", hostIp=" + hostIp + ", openSw=" + openSw + ", readNum=" + readNum + ", wDate=" + wDate
-				+ ", good=" + good + ", complaint=" + complaint + "]";
+				+ ", good=" + good + ", complaint=" + complaint + ", hour_diff=" + hour_diff + ", date_diff=" + date_diff
+				+ ", replyCnt=" + replyCnt + "]";
 	}
 }
